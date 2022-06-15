509b5f1f226d01d125eba372c94ef90499520a59
jeremiasseleme <seleme.jeremias@gmail.com>
2022-06-15 13:18:17 -0300

    Crea tu landing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import Nav from './Components/Nav';
 
+
 function App() {
-  return <Nav/>;
+  return (
+  <>
+    <Nav />
+    <ItemListContainer greeting="Bienvenido a mi lista de productos" />
+  </>
+  )
 }
 
 export default App;
---
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function ItemListContainer({greeting}) {
+  return (
+    <span>{greeting}</span>
+    
+  )
+}
